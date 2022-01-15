2588aae44396f3f06ff5d35bbd7702348af4634c
kouya17 <kouya17@users.noreply.github.com>
2022-01-15 04:54:14 +0000

    deploy: 737786e1865a43f01d37a63d1e9547582ddbf146

--- _next/static/DhZ-e1htG2iBiz8YQs_IZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b282b959608f58d4.css","static/chunks/pages/index-e0fd32851bd059a8.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
