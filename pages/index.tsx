a57a72dd432392a62a3535c6449c80a225b17ec9
kouya17 <17383696+kouya17@users.noreply.github.com>
2022-01-15 13:53:06 +0900

    feat: add github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <YouTube />
+      <br/>
+      <a href="https://github.com/kouya17/youtube-iframe">Source code</a>
     </div>
   )
 }
